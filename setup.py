9bc12a69e68dabe87f929b7f4acec1bcb228d439
saurabh kumar <sgithub9572@gmail.com>
2025-04-06 00:43:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+# This function will return a list of requirements from the given file path
+
+##HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return a list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+
+setup(
+
+    name="ML-Project",
+    version="0.0.1",
+    author="Saurabh Kumar",
+    author_email="sgithub8072@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
